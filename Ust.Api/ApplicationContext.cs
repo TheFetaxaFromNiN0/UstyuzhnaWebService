c1b268b6be6f025f319c882504289cd9d658b561
Mihail <mikhail.golubev@firstlinesoftware.com>
2020-03-23 22:42:38 +0300

    add

--- Ust.Api/ApplicationContext.cs
+++ Ust.Api/ApplicationContext.cs
@@ -36,10 +36,10 @@ namespace Ust.Api
             //Album
             modelBuilder.Entity<Album>().HasIndex(a => a.Name).IsUnique();
             modelBuilder.Entity<File>().Property(a => a.CreatedDate).HasDefaultValueSql("getdate()");
-            //Для каждой Сущности у которой файлы = таблицу связи
             //File
             modelBuilder.Entity<File>().HasKey(f => new {f.Id});
             modelBuilder.Entity<File>().Property(f => f.CreatedDate).HasDefaultValueSql("getdate()");
+            //Для каждой Сущности у которой файлы = таблицу связи
 
         }
 
--- Ust.Api/Models/ModelDbObject/OrganizationFile.cs
+++ Ust.Api/Models/ModelDbObject/OrganizationFile.cs
@@ -22,4 +22,3 @@ namespace Ust.Api.Models.ModelDbObject
 
     }
 }
-}
